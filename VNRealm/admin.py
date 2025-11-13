8a0a01317a3ebbb1e0acddd8d8b68b1ce0d1a827
VinushaMuthusamy <vinushamuthusamy@cmail.carleton.ca>
2025-11-13 15:12:23 -0500

    save

--- VNRealm/admin.py
+++ VNRealm/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import VisualNovels
+from .models import Users
+# Replace ModelName in the above line
+# with the name of your model in models.py
+
+admin.site.register(VisualNovels)
+# Replace ModelName in the above line
+# with the name of your model in models.py
+
+admin.site.register(Users)
--- VNRealm/models.py
+++ VNRealm/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class VisualNovels(models.Model):
     def __str__(self):
         return self.author
+class Users(models.Model):
+    def __str__(self):
+        return self.threads
