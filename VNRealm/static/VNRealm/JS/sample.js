fdb09dcb1ca2845b2ba60979c2596ca8a4e6cd1d
VinushaMuthusamy <vinushamuthusamy@cmail.carleton.ca>
2025-11-27 13:14:31 -0500

    popupwindow

--- VNRealm/static/VNRealm/JS/sample.js
+++ VNRealm/static/VNRealm/JS/sample.js
@@ -2,17 +2,38 @@
 
 
 console.log("hi")
+ document.addEventListener("DOMContentLoaded", function() {
 
+const form = document.getElementById("reviewForm");
+ const popup = document.getElementById("authPopup");
+  const loginBtn = document.getElementById("loginBtn");
+    const closeBtn = document.getElementById("closePopup");
+     if (!form) return; // safety check
+  const isAuthenticated = form.dataset.auth;
+  form.addEventListener("submit", function(event) {
+       if (isAuthenticated === "False") {
 
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('reviewform');
-    form.addEventListener('submit', (e) => {
-        // Check if user is logged in
-        const isAuthenticated = "{{ user.is_authenticated|yesno:'True,False' }}";
-        if (isAuthenticated === "False") {
-            e.preventDefault(); // stop the form submission
-            alert("You must log in to post a review!");
-           // window.location.href = '/login/?next=' + window.location.pathname;
-        }
+           event.preventDefault(); // stops the redirect to /otherpage/
+           console.log("Form submission prevented!");
+           //alert("gotta login");
+           popup.style.display = "block";
+
+       }
+    // You can now handle the data with JS (AJAX, validation, etc.)
+  });
+  closeBtn.addEventListener("click", function() {
+        popup.style.display = "none"; // hide popup
+
     });
-});
+  window.addEventListener("click", function(e) {
+        if (e.target == popup) {
+            popup.style.display = "none";
+        }
+        });
+   if (loginBtn) {
+        loginBtn.addEventListener("click", function() {
+            window.location.href = loginBtn.dataset.loginUrl;
+        });
+    }
+
+ });
