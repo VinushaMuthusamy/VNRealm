d4958a4c154dbd9ffa07abf32848bbf0ec91ec6c
VinushaMuthusamy <vinushamuthusamy@cmail.carleton.ca>
2025-11-28 18:51:58 -0500

    comments

--- VNRealm/static/VNRealm/JS/sample.js
+++ VNRealm/static/VNRealm/JS/sample.js
@@ -1,17 +1,25 @@
 
 
-
+//loading  test
 console.log("hi")
  document.addEventListener("DOMContentLoaded", function() {
 
-const form = document.getElementById("reviewForm");
- const popup = document.getElementById("authPopup");
-  const loginBtn = document.getElementById("loginBtn");
+     //review post
+    const form = document.getElementById("reviewForm");
+    //authentication pop window
+    const popup = document.getElementById("authPopup");
+    //login/signup page redirection
+    const loginBtn = document.getElementById("loginBtn");
+    //closing popup
     const closeBtn = document.getElementById("closePopup");
-    //check
+    //check if no form
      if (!form) return;
+     //dataset reads metadata info such as authentication state
   const isAuthenticated = form.dataset.auth;
+
+  //pop up window function
   form.addEventListener("submit", function(event) {
+      //if not authenticated window will appear
        if (isAuthenticated === "False") {
 
            // stops the redirect to login page
@@ -22,15 +30,20 @@ const form = document.getElementById("reviewForm");
 
        }
   });
+  //clicking close button in the window will close it
   closeBtn.addEventListener("click", function() {
         popup.style.display = "none"; // hide popup
 
     });
+
+  //clicking outside the window will also close it
   window.addEventListener("click", function(e) {
         if (e.target === popup) {
             popup.style.display = "none";
         }
         });
+
+  //if login/signup button clicked it would redirect to login page
    if (loginBtn) {
         loginBtn.addEventListener("click", function() {
             window.location.href = loginBtn.dataset.loginUrl;
